b13b6fb4c7abf22991951273a76d61b2a9b84623
Gyorgy0 <lukovics.gyuri@gmail.com>
2024-06-26 16:33:30 +0200

    Added the game board and the program can display it.

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,59 @@
-use macroquad::{prelude::*};
+use macroquad::prelude::*;
 
 #[macroquad::main("Particle Simulator")]
 
 async fn main() {
-   	loop
-	{
-		clear_background(BLUE);
-		for i in 1..=(screen_height() as u32/CELLSIZE as u32) as i32 {
-			for j in 1..=(screen_width() as u32/CELLSIZE as u32) as i32  {
-			}
-		}
-		next_frame().await;
-   	}
+    let vacuum = Particle(VOID, vec2(0.0, 0.0), vec2(0.0, 0.0));
+    let cell_count =
+        ((screen_height() / CELLSIZE as f32) * (screen_width() / CELLSIZE as f32)) as i32;
+    let row_count = (screen_height() / CELLSIZE as f32) as i32;
+    let col_count = (screen_width() / CELLSIZE as f32) as i32;
+    let mut game_board: Vec<Particle> = vec![vacuum; cell_count as usize];
+    for i in 1..row_count {
+        for j in 1..col_count {
+            game_board[(i * col_count + j) as usize] =
+                Particle(VOID, vec2(j as f32, i as f32), vec2(0.0, 0.0));
+        }
+    }
+    loop {
+        clear_background(RED);
+        DrawBoard(&game_board, row_count, col_count);
+        next_frame().await;
+    }
 }
 
-const CELLSIZE:u32 = 10;
-
-const GRAVITY:f32 = 9.81;
-const FPS:u32 = 60;
-
-static WATER:Particle = Particle
-{
-	material_id: 1,
-	mass: 1.0,
-	viscosity: 1.0,
-	color: BLUE,
-};
-
-#[derive(Copy, Clone)]
-struct Particle 
-{
-	material_id: u16, // the identification number that helps identifying the material in the cell array
-	mass: f32, // mass of a cm^3 volume of the material
-	viscosity: f32, // viscosity of the material -> higher number = thicker material (viscosity of water is 1)
-	color: Color, // color of the material
+fn DrawBoard(game_board: &Vec<Particle>, row_count: i32, col_count: i32) {
+    for i in 1..row_count {
+        for j in 1..col_count {
+            let cell: Particle = game_board[((i * col_count) + j) as usize];
+            draw_rectangle(
+                cell.1.x * CELLSIZE as f32,
+                cell.1.y * CELLSIZE as f32,
+                CELLSIZE as f32,
+                CELLSIZE as f32,
+                cell.0 .2,
+            )
+        }
+    }
 }
+
+const CELLSIZE: u32 = 5;
+
+const GRAVITY: f32 = 9.81;
+const FPS: u32 = 60;
+
+static VOID: Material = Material(0.0, 0.0, color_u8!(0.0, 0.0, 0.0, 100.0));
+
+static WATER: Material = Material(1.0, 1.0, BLUE);
+
+#[derive(PartialEq, Debug, Copy, Clone, Default)]
+struct Material(f32, f32, Color);
+// 0 (f32) - 	mass of a cm^3 volume of the material
+// 1 (f32) - 	viscosity of the material -> higher number = thicker material (viscosity of water is 1)
+// 2 (Color) - 	color of the material
+
+#[derive(PartialEq, Debug, Clone, Copy, Default)]
+struct Particle(Material, Vec2, Vec2);
+// 0 (Material) - 	material of the particle
+// 1 (Vec2) - 		visco
+// 2 (Vec2) - 		color of the material
