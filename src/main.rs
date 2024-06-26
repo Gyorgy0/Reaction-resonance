10b97ca5e519af9ea9680724d8a6c7a644955430
Gyorgy0 <lukovics.gyuri@gmail.com>
2024-06-27 01:45:38 +0200

    Reworked the Material struct

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use macroquad::prelude::*;
+use miniquad::window::set_fullscreen;
 
 #[macroquad::main("Particle Simulator")]
 
 async fn main() {
-    let vacuum = Particle(VOID, vec2(0.0, 0.0), vec2(0.0, 0.0));
     let cell_count =
         ((screen_height() / CELLSIZE as f32) * (screen_width() / CELLSIZE as f32)) as i32;
     let row_count = (screen_height() / CELLSIZE as f32) as i32;
     let col_count = (screen_width() / CELLSIZE as f32) as i32;
-    let mut game_board: Vec<Particle> = vec![vacuum; cell_count as usize];
+    let mut game_board: Vec<Particle> = vec![Particle(VOID, vec2(0.0, 0.0), vec2(0.0, 0.0)); cell_count as usize];
     for i in 1..row_count {
         for j in 1..col_count {
             game_board[(i * col_count + j) as usize] =
@@ -22,6 +22,7 @@ async fn main() {
     }
 }
 
+
 fn DrawBoard(game_board: &Vec<Particle>, row_count: i32, col_count: i32) {
     for i in 1..row_count {
         for j in 1..col_count {
@@ -31,26 +32,37 @@ fn DrawBoard(game_board: &Vec<Particle>, row_count: i32, col_count: i32) {
                 cell.1.y * CELLSIZE as f32,
                 CELLSIZE as f32,
                 CELLSIZE as f32,
-                cell.0 .2,
+                cell.0.color,
             )
         }
     }
 }
 
-const CELLSIZE: u32 = 5;
+const CELLSIZE: u32 = 1;
 
 const GRAVITY: f32 = 9.81;
 const FPS: u32 = 60;
 
-static VOID: Material = Material(0.0, 0.0, color_u8!(0.0, 0.0, 0.0, 100.0));
+static VOID: Material = Material
+{
+    mass: 0.0, 
+    viscosity: 0.0, 
+    color: color_u8!(0.0, 0.0, 0.0, 100.0)
+};
 
-static WATER: Material = Material(1.0, 1.0, BLUE);
+static WATER: Material = Material
+{
+    mass: 1.0, 
+    viscosity: 1.0, 
+    color: BLUE
+};
 
 #[derive(PartialEq, Debug, Copy, Clone, Default)]
-struct Material(f32, f32, Color);
-// 0 (f32) - 	mass of a cm^3 volume of the material
-// 1 (f32) - 	viscosity of the material -> higher number = thicker material (viscosity of water is 1)
-// 2 (Color) - 	color of the material
+struct Material {
+    mass: f32,  // mass of a cm^3 volume of the material
+    viscosity: f32, // viscosity of the material -> higher number = thicker material (viscosity of water is 1)
+    color: Color // color of the material
+}
 
 #[derive(PartialEq, Debug, Clone, Copy, Default)]
 struct Particle(Material, Vec2, Vec2);
