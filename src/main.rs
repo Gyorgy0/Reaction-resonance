b1a3f6e38695ff8284a7b43c91dbf5eba61fdb28
Gyorgy0 <lukovics.gyuri@gmail.com>
2024-07-07 19:39:13 +0200

    Added sand + phase to materials

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,12 @@ use macroquad::prelude::*;
 #[macroquad::main("Particle Simulator")]
 
 async fn main() {
-    let mut start:bool = true;
+    let mut start: bool = true;
     let mut game_board = vec![];
     let row_count = 50;
     let col_count = 50;
     loop {
-        if start
-        {
+        if start {
             game_board = SetupBoard(row_count, col_count);
             start = false;
         }
@@ -20,10 +19,10 @@ async fn main() {
     }
 }
 
-fn SetupBoard(row_count: i32, col_count: i32) -> Vec<Particle> 
-{
-    let cell_count = row_count*col_count;
-    let mut game_board: Vec<Particle> = vec![Particle(VOID, vec2(0.0, 0.0), false); cell_count as usize];
+fn SetupBoard(row_count: i32, col_count: i32) -> Vec<Particle> {
+    let cell_count = row_count * col_count;
+    let mut game_board: Vec<Particle> =
+        vec![Particle(VOID, vec2(0.0, 0.0), false); cell_count as usize];
     for i in 0..row_count {
         for j in 0..col_count {
             game_board[(i * col_count + j) as usize] = Particle(VOID, vec2(0.0, 0.0), false);
@@ -51,69 +50,96 @@ fn UpdateBoard(game_board: &mut Vec<Particle>, row_count: i32, col_count: i32) -
     for i in 0..row_count {
         for j in 0..col_count {
             let mut cell: Particle = game_board[((i * col_count) + j) as usize];
-            cell.1.y += (cell.0.mass* GRAVITY * get_frame_time());
-            if (cell.0.mass > 0.0){
-                if (i + (cell.1.y as i32 - 1)) < row_count-1 && (!cell.2) && cell.1.y > 1.0 {
+            cell.1.y += (cell.0.mass * GRAVITY * get_frame_time());
+            if (cell.0.mass > 0.0) {
+                if (i + (cell.1.y as i32 - 1)) < row_count - 1 && (!cell.2) && cell.1.y > 1.0 {
                     let tmp: Particle =
                         game_board[(((i + cell.1.y as i32) * col_count) + j) as usize];
                     game_board[(((i + cell.1.y as i32) * col_count) + j) as usize] = cell;
                     game_board[(((i + cell.1.y as i32) * col_count) + j) as usize].2 = true;
                     game_board[((i * col_count) + j) as usize] = tmp;
-                }
-                else if ((i as i32 - 1) - (row_count-1)) < cell.1.y as i32 && (!cell.2) && cell.1.y > 1.0 {
-                    cell.1.y = (-1.0)* ((i as i32) - (row_count-1)) as f32;
-                    let tmp: Particle =
-                        game_board[(((i + cell.1.y as i32) * col_count) + j) as usize];
-                    game_board[(((i + cell.1.y as i32) * col_count) + j) as usize] = cell;
-                    game_board[(((i + cell.1.y as i32) * col_count) + j) as usize].1.y = 0.0;
-                    game_board[(((i + cell.1.y as i32) * col_count) + j) as usize].2 = true;
-                    game_board[((i * col_count) + j) as usize] = tmp;
-                }
-                else if (cell.1.y < 1.0) 
+                } else if ((i as i32 - 1) - (row_count - 1)) < cell.1.y as i32
+                    && (!cell.2)
+                    && cell.1.y > 1.0
                 {
+                    cell.1.y = (-1.0) * ((i as i32) - (row_count - 1)) as f32;
+                    let tmp: Particle =
+                        game_board[(((i + cell.1.y as i32) * col_count) + j) as usize];
+                    game_board[(((i + cell.1.y as i32) * col_count) + j) as usize] = cell;
+                    game_board[(((i + cell.1.y as i32) * col_count) + j) as usize]
+                        .1
+                        .y = 0.0;
+                    game_board[(((i + cell.1.y as i32) * col_count) + j) as usize].2 = true;
+                    game_board[((i * col_count) + j) as usize] = tmp;
+                } else if (cell.1.y < 1.0) {
                     game_board[((i * col_count) + j) as usize].1.y = cell.1.y;
                 }
+            }
         }
     }
-}
     for i in 0..row_count {
         for j in 0..col_count {
             game_board[((i * col_count) + j) as usize].2 = false;
         }
     }
-    let btn:MouseButton = MouseButton::Left;
-    if is_mouse_button_down(btn)
+    let btn: MouseButton = MouseButton::Left;
+    let rbtn: MouseButton = MouseButton::Right;
+    if is_mouse_button_down(btn) {
+        let cursor_position = mouse_position();
+        if (cursor_position.0 > 10.0
+            && cursor_position.0 < 510.0
+            && cursor_position.1 > 10.0
+            && cursor_position.1 < 510.0)
         {
-            let cursor_position = mouse_position();
-            if (cursor_position.0 >= 10.0 && cursor_position.0 <= 510.0 && cursor_position.1 >= 10.0 && cursor_position.1 <= 510.0) 
-            {
-                let x = cursor_position.0 as u32 / CELLSIZE;
-                let y = cursor_position.1 as u32 / CELLSIZE;
-                game_board[(y * col_count as u32 + x) as usize] = Particle(WATER, vec2(0.0, 0.0), false);
-            }
+            let x = (cursor_position.0 as u32 / CELLSIZE) - 1;
+            let y = (cursor_position.1 as u32 / CELLSIZE) - 1;
+            game_board[(y * col_count as u32 + x) as usize] =
+                Particle(WATER, vec2(0.0, 0.0), false);
         }
+    }
+    if is_mouse_button_down(rbtn) {
+        let cursor_position = mouse_position();
+        if (cursor_position.0 > 10.0
+            && cursor_position.0 < 510.0
+            && cursor_position.1 > 10.0
+            && cursor_position.1 < 510.0)
+        {
+            let x = (cursor_position.0 as u32 / CELLSIZE) - 1;
+            let y = (cursor_position.1 as u32 / CELLSIZE) - 1;
+            game_board[(y * col_count as u32 + x) as usize] = Particle(SAND, vec2(0.0, 0.0), false);
+        }
+    }
     return game_board.to_vec();
 }
 
-const FPS: u32 = 60;
 const CELLSIZE: u32 = 10;
-const GRAVITY:f32 = 9.81;
+const GRAVITY: f32 = 9.81;
 
 static VOID: Material = Material {
     mass: 0.0,
+    phase: 0,
     viscosity: 0.0,
     color: color_u8!(0.0, 0.0, 0.0, 100.0),
 };
 
 static WATER: Material = Material {
     mass: 1.0,
+    phase: 3,
     viscosity: 1.0,
     color: BLUE,
+};
+
+static SAND: Material = Material {
+    mass: 1.682,
+    phase: 2,
+    viscosity: 0.0,
+    color: color_u8!(203, 189, 147, 255),
 };
 
 #[derive(PartialEq, Debug, Copy, Clone, Default)]
 struct Material {
     mass: f32,      // mass of a cm^3 volume of the material
+    phase: u8, // phase of the material    -> 0 - void, 1 - solid, 2 - powder, 3 - liquid, 4 - gas, 5 - plasma
     viscosity: f32, // viscosity of the material -> higher number = thicker material (viscosity of water is 1)
     color: Color,   // color of the material
 }
